c769a3115822e7cbf618f129cb54d419145c15b9
aleworth <austinaylworth@gmail.com>
2021-09-21 12:13:13 -0500

    Add files via upload

---
+++ hybridBuild.ps1
@@ -0,0 +1,14 @@
+$scheme
+$wshell = New-Object -ComObject Wscript.Shell
+
+npm run package
+npm run platform:android
+Set-Location .\build\
+cordova plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=$scheme
+cordova plugin add cordova-plugin-browser
+Set-Location ..
+npm run package
+npm run prepare:android
+npm run build
+
+$Output = $wshell.Popup("The hybrid build has completed!")
